976c2897ead50c8d7fdc5f8a1804ccdd964da1ca
Eugene Checkoy <mfalcon@mail.ru>
2022-10-19 12:46:32 +0300

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py migrate --run-syncdb
 
 python manage.py collectstatic --no-input
 
-gunicorn spirits.wsgi:application --bind 0.0.0.0:8080 --reload  -w 4
+gunicorn SportNews.wsgi:application --bind 0.0.0.0:8080 --reload  -w 4
